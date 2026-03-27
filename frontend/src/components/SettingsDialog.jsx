301eb71cb722f1bc76ee66527dd7f22fc7cc2c16
GreatestFool <3003484+GreatestFool@users.noreply.github.com>
2026-03-27 11:23:00 +0100

    Don't look at me, the electric bogaloo

--- frontend/src/components/SettingsDialog.jsx
+++ frontend/src/components/SettingsDialog.jsx
@@ -172,6 +172,7 @@ export default function SettingsDialog({ open, onClose }) {
           options={[
             { value: "fr", label: "🇫🇷 Français" },
             { value: "en", label: "🇬🇧 English" },
+            { value: "no", label: "🇳🇴 Norwegian Bokmål" },
           ]}
           style={{ width: 160 }}
         />
